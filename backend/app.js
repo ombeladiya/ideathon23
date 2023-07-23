6f76bbe41eb8efa5358cab2c240e1badc202cc33
ombeladiya <ombeladiya1854@gmail.com>
2023-07-23 12:42:34 +0530

    hdfurh

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express =require("express");
 const app=express();
-
+const path = require("path");
 
 if (process.env.NODE_ENV !== "PRODUCTION") {
     require("dotenv").config({ path: "backend/config/config.env" });
