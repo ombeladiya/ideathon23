c2a56659a2bf7747e3ea44a8a262a394c7c50148
ombeladiya <ombeladiya1854@gmail.com>
2023-07-25 21:38:05 +0530

    ufhu

--- frontend/src/components/Schedule.jsx
+++ frontend/src/components/Schedule.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import sch from "./shedule.png";
+import sc from "./schedul.jpg";
 function Schedule() {
   return (
     <div style={{minHeight:"65vh"}}>
       <center>
         <div>
           <img src={sch} alt="schedule" />
+          <img src={sc} alt="schedule" />
         </div>
       </center> 
     </div>
